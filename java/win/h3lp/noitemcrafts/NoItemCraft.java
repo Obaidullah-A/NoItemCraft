bbcc0daff06f32d7ce59c7b8821121b18665084e
Master Factions <114424563+Obaidullah-A@users.noreply.github.com>
2024-06-11 08:40:17 -0400

    initial commit

---
+++ java/win/h3lp/noitemcrafts/NoItemCraft.java
@@ -0,0 +1,17 @@
+package win.h3lp.noitemcraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NoItemCraft extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
